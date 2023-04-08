f608c820b9a1facc76a43f4aed07fdfd926ce94f
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-04-08 19:30:02 +0530

    Add Pending Approved and Rejected Tabs

--- frontend/src/components/Management.jsx
+++ frontend/src/components/Management.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Flex, FormControl, FormHelperText, FormLabel, HStack, Heading, Input, Select, Stack } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, FormControl, FormHelperText, FormLabel, HStack, Heading, Input, Select, Stack, Tab, TabIndicator, TabList, Tabs } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import TaskList from "./TaskList";
 import DisplayLayout from "./DisplayLayout";
@@ -7,6 +7,8 @@ import { useLocation } from "react-router-dom";
 import { wrap } from "framer-motion";
 const Management = () => {
      const [list, setList] = useState([]);
+     const [filteredList, setFilteredList] = useState([]);
+     const [tabIndex, setTabIndex] = useState(0);
      const location = useLocation();
      // console.log("location", location);
      const { name, userType, email, _id, token } = location.state;
@@ -18,6 +20,8 @@ const Management = () => {
                     // console.log("getList---", list);
                     setList(list);
                })();
+               let filtrList = list.filter((value) => value.status === "Pending");
+               setFilteredList(filtrList);
           }
      }, [name]);
 
@@ -27,14 +31,37 @@ const Management = () => {
           setList([...filterList, newList]);
      };
 
+     const handleFilterList = (index) => {
+          if (index === 0) {
+               let filtrList = list.filter((value) => value.status === "Pending");
+               setFilteredList(filtrList);
+          } else if (index === 1) {
+               let filtrList = list.filter((value) => value.status === "Approved");
+               setFilteredList(filtrList);
+          } else if (index === 2) {
+               let filtrList = list.filter((value) => value.status === "Rejected");
+               setFilteredList(filtrList);
+          }
+     };
+
      return (
           <Box flexDirection={"column"} display="flex" gap={4} alignItems={"center"}>
                <DisplayLayout role={userType} name={name} email={email} />
                <Divider />
+               <Box w={["90%", "80%", "70%"]}>
+                    <Tabs isFitted variant="line" onChange={handleFilterList}>
+                         <TabList mb="1em">
+                              <Tab>Pending</Tab>
+                              <Tab>Approved</Tab>
+                              <Tab>Rejected</Tab>
+                         </TabList>
+                    </Tabs>
+               </Box>
+
                <Flex gap={3} m={3} flexWrap={"wrap"}>
-                    {list.map((value) => (
-                         <TaskList {...value} userType={userType} key={value._id} handleRejectOrApprovedList={handleRejectOrApprovedList} />
-                    ))}
+                    {filteredList.length !== 0 &&
+                         filteredList.map((value) => <TaskList {...value} userType={userType} key={value._id} handleRejectOrApprovedList={handleRejectOrApprovedList} />)}
+                    {filteredList.length === 0 && <Heading size="sm">No List Found</Heading>}
                </Flex>
           </Box>
      );
