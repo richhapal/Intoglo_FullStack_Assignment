705e3b6d4422227976c602128071254c0d78967c
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-04-08 19:00:23 +0530

    List UI Change

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,40 +1,63 @@
-import { Box, Button, Flex, HStack, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, Heading, Text, VStack } from "@chakra-ui/react";
 import React from "react";
-
+import { updateList } from "../apiHandler/apiHandler";
 const TaskList = (props) => {
-     const { userName, amount, assignto, status, comment, userType } = props;
-     const handleApproved = () => {};
-     const handleRejected = () => {};
+     const { name, amount, assignTo, status, comment, userType, date, _id, handleRejectOrApprovedList } = props;
+     let token = localStorage.getItem("token");
+     const handleApproved = async () => {
+          const data = {
+               listId: _id,
+               status: "Approved",
+               comment: `Approved by ${userType} on ${new Date().toDateString()} @ ${new Date().toLocaleTimeString()}`,
+          };
+          let list = await updateList(data, token);
+          handleRejectOrApprovedList(list);
+          // window.location.reload();
+     };
+     const handleRejected = async () => {
+          const data = {
+               listId: _id,
+               status: "Rejected",
+               comment: `Rejected by ${userType} on ${new Date().toDateString()} @ ${new Date().toLocaleTimeString()}`,
+          };
+          let list = await updateList(data, token);
+          handleRejectOrApprovedList(list);
+          // window.location.reload();
+     };
 
      return (
-          <Box border="2px" w="90%" borderRadius="md" padding={3}>
-               <Flex justifyContent="space-evenly">
+          <Box border="2px" w={"sm"} maxWidth={"sm"} borderRadius="md" padding={3}>
+               <VStack align gap={1}>
                     <HStack>
                          <Text>UserName :</Text>
-                         <Heading size="sm">{userName}</Heading>
+                         <Heading size="sm">{name}</Heading>
                     </HStack>
                     <HStack>
                          <Text>Amount :</Text>
                          <Heading size="sm">{amount}</Heading>
                     </HStack>
                     <HStack>
-                         <Text>AssignTo :</Text>
-                         <Heading size="sm">{assignto}</Heading>
+                         <Text>Date :</Text>
+                         <Heading size="sm">{new Date(date).toDateString("en-IN")}</Heading>
                     </HStack>
-                    {userType === "User" && status === "Pending" && (
+                    <HStack>
+                         <Text>AssignTo :</Text>
+                         <Heading size="sm">{assignTo}</Heading>
+                    </HStack>
+                    {status === "Pending" && (
                          <HStack>
                               <Text>Status :</Text>
                               <Heading size="sm">{status}</Heading>
                          </HStack>
                     )}
 
-                    {status === "Approved" && (
+                    {(status === "Approved" || status === "Rejected") && (
                          <HStack>
-                              <Text>Approved By:</Text>
-                              <Heading size="sm">{comment}</Heading>
+                              <Text>Status:</Text>
+                              <Heading size="xs">{comment}</Heading>
                          </HStack>
                     )}
-                    {userType !== "User" && status === "Pending" && (
+                    {userType !== "user" && status === "Pending" && (
                          <HStack>
                               <Button colorScheme="green" onClick={handleApproved}>
                                    Approve
@@ -44,7 +67,7 @@ const TaskList = (props) => {
                               </Button>
                          </HStack>
                     )}
-               </Flex>
+               </VStack>
           </Box>
      );
 };
